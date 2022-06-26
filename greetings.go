150d111db5de91031cf130e8e2daf21070049a0b
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2022-06-25 20:21:21 -0500

    Fix: Handle errors
    Now an error is return if the name variable is empy

--- greetings.go
+++ greetings.go
@@ -1,26 +1,35 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 /*
 
-This is par of "Get started" tutorial from go.dev
+This is part of "Get started" tutorial from go.dev
 
-Just call this function to you main code and print a nice greeting
+Just call this function to your main code and print a nice greeting
 
 */
 
 // Hello returns a greeting for the named person
-func Hello(name string) string {
-  // Return a greeting tahat enbeds the name in a message
-  message := fmt.Sprintf("✋ Hi, %v. Welcome!", name)
-  return message
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a neme was received, retun a value that embeds the name
+	// in a greeting message
+	message := fmt.Sprintf("✋ Hi, %v. Welcome!", name)
+	return message, nil
 }
 
 /*
 
 Note (for myself):
 In Go, a function whose name starts with a capital letter
-can be called ba a function not in the same package
+can be called by a function not in the same package
 
 */
