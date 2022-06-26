79441600b274d63ce3e8b1ad13ce438183a31e43
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2022-06-25 21:39:52 -0500

    Changed: Return a random greeting
    Now the function hello returns a random greeteing defined from slice

--- greetings.go
+++ greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 /*
@@ -20,16 +22,39 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 
-	// If a neme was received, retun a value that embeds the name
-	// in a greeting message
-	message := fmt.Sprintf("✋ Hi, %v. Welcome!", name)
+	// Create a message using a random format
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// init sets initial values for variables used in the function
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at reandom
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"👋 Hi, %v. Welcome!",
+		"✌ Great to see you, %v!",
+		"✋ Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
 
 /*
 
 Note (for myself):
-In Go, a function whose name starts with a capital letter
+- In Go, a function whose name starts with a capital letter
 can be called by a function not in the same package
+
+- Add an init function to seed the rand package with the current time.
+Go executes init functions automatically at program startup, after global
+variables have been initialized.
 
 */
